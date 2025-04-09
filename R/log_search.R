f6a32e650d5dde9f531aa10b81e3786737df8169
kengreklammen <6339237+kengreklammen@users.noreply.github.com>
2025-04-09 15:39:42 +0200

    Fixing the return value in the log_search function.

--- R/log_search.R
+++ R/log_search.R
@@ -7,7 +7,7 @@ library(RPostgres)
 #' @param user_id A numeric type user ID.
 #' @param query The search string entered by the user.
 #'
-#' @returns Nothing.
+#' @returns TRUE if the insert was successful, FALSE if otherwise.
 #' @export
 #'
 #' @examples
@@ -18,10 +18,16 @@ library(RPostgres)
 #' }
 log_search <- function(user_id = NULL, query = NULL){
 	if(!is.numeric(user_id) || is.null(user_id) || is.numeric(query) || is.null(query)){stop("Missing parameter, or wrong parameter type!")}
-	con <- connect_db()
-	sql <- glue::glue_sql("insert into student_arpad.search_logs (user_id, query) values({user_id}, {query});", .con = con)
-	DBI::dbExecute(con, "SET search_path TO student_arpad")
-	df <- DBI::dbExecute(con, sql)
-	DBI::dbDisconnect(con)
-	return(df)
+	ret <- FALSE
+	result <- tryCatch({
+		con <- connect_db()
+		sql <- glue::glue_sql("insert into student_arpad.search_logs (user_id, query) values({user_id}, {query});", .con = con)
+		rows_affected <- DBI::dbExecute(con, "SET search_path TO student_arpad")
+		df <- DBI::dbExecute(con, sql)
+		DBI::dbDisconnect(con)
+		ret <- TRUE
+	}, error = function(e) {
+		message("Couldn't insert the search query into the database: ", e$message)
+	})
+	return(ret)
 }
